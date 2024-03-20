e81ac47ffc6a6ab614e7a5de77013efc42620dda
김범기 <dyd1308@naver.com>
2024-03-20 21:37:12 +0900

    redux-toolkit을 통한 store 설정 및 usestate 적용

--- FrontEnd/tarotmilktea/src/components/pages/Home.jsx
+++ FrontEnd/tarotmilktea/src/components/pages/Home.jsx
@@ -1,13 +1,20 @@
 import './Home.css';
-import React from 'react';
+import React, {useState} from 'react';
 import { Link, useNavigate } from 'react-router-dom'
-// import { ReactComponent as Logo } from "../../assets/images/Logo.svg";
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement, incrementByAmount } from '../../redux/slices/main/mainSlice'
 import TarotMilkTeaLogo from "../../assets/images/TarotMilkTeaLogo.png"
 
 
 function Home() {
   let navigate = useNavigate()
+  const counts = useSelector((state) => state.mainSlice.value)
+  const dispatch = useDispatch()
+  const [tmpNum, setTmpNum] = useState(0)
 
+  const handleInputChange = (e) => {
+    setTmpNum(Number(e.target.value))
+  }
   return (
     <div className='MainContainer'>
       {/* 상단 로고 부분 */}
@@ -38,6 +45,15 @@ function Home() {
       </nav>
       {/* 메인페이지 부분 */}
       <div></div>
+      {/* 테스트 부분 */}
+      <div>
+        <input type="number" onChange={handleInputChange} value={tmpNum} />
+
+        <p>표시 숫자: {counts}</p>
+        <button onClick={()=>{return dispatch(increment())}}>증가버튼</button>
+        <button onClick={()=>{return dispatch(decrement())}}>감소버튼</button>
+        <button onClick={()=>{return dispatch(incrementByAmount(tmpNum))}}>숫자증감버튼</button>
+      </div>
     </div>
   );
 }
